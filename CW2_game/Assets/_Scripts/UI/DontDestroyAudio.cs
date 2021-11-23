4137158620d383a9ff56110c6df7918cb5280805
dinilwick <dsw38@bath.ac.uk>
2021-11-23 21:23:21 +0000

    Scripts for Intro Screen
    Doesnt destroy audio, switches scenes, sound management

---
+++ CW2_game/Assets/_Scripts/UI/DontDestroyAudio.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyAudio : MonoBehaviour
+{
+    static DontDestroyAudio instance = null;
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
