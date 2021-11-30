bf7cf46a645fa4e6060ab20654b3cc3d02daa979
gupta.vinay <vg447@bath.ac.uk>
2021-11-30 18:40:01 +0000

    Added Win and Lose screen with logic

---
+++ CW2_game/Assets/_Scripts/GameOver/GameOverLostScreen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverLostScreen : MonoBehaviour
+{
+    public void Setup()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        gameObject.SetActive(false);
+        SceneManager.LoadScene("UnitTestEnv");
+    }
+
+    public void GoToMainMenu()
+    {
+        gameObject.SetActive(false);
+        SceneManager.LoadScene("MapMenu");
+    }
+}
